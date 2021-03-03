8c6e222bb3b7a88f76920068ccd9e1ea64a456cd
이주연/선임연구원/SW Platform(연)Platform Core TP(jylee256.lee@lge.com) <jylee256.lee@lge.com>
2021-03-03 14:24:37 +0900

    delete

--- myss/js/util.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-function randomNumber(strLength) {
-    var value = "";
-    for (var i=0; i<strLength; i++) {
-        value += parseInt(Math.random() * 10);
-    }
-    return value;
-}
